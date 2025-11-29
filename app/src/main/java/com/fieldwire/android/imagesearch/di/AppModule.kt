3921b8553da84d9553055f2b128e0bee661c5644
Hamza Khan <hamzak@umich.edu>
2025-11-28 22:47:36 -0500

    added AppModule

---
+++ app/src/main/java/com/fieldwire/android/imagesearch/di/AppModule.kt
@@ -0,0 +1,60 @@
+package com.fieldwire.android.imagesearch.di
+
+import com.fieldwire.android.imagesearch.api.ImgurApi
+import com.fieldwire.android.imagesearch.data.ImageRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+private const val IMGUR_API_BASE_URL = " https://api.imgur.com/3/"
+private const val CLIENT_ID = "b067d5cb828ec5a"
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient() : OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("Authorization", "Client-ID $CLIENT_ID")
+                    .build()
+                chain.proceed(request)
+            }
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient) : Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(IMGUR_API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providerImgurApi(retrofit: Retrofit) : ImgurApi {
+        return retrofit.create(ImgurApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideImageRepository(imgurApi: ImgurApi): ImageRepository {
+        return ImageRepository(imgurApi)
+    }
+}
